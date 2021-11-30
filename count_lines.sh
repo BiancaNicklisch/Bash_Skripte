ca543278b483d71f33aa0a1c46aa0baa36f444df
Bianca Nicklisch <nicklisch@mpisoc.mpg.de>
2021-11-30 10:00:52 +0100

    Add bash script

---
+++ count_lines.sh
@@ -0,0 +1,5 @@
+# Ein kleines Script zum Zählen von Zeilen in TSV-Datein
+# 2021 - Bianca Nicklisch  <nicklisch@mpisoc.mpg.de>
+echo "Count lines in CSV files"
+wc -l *tsv
+echo "Done! Have a nice day"
